8c02aa10ec2f2a920b2cd7bf6491f74735e9929f
manhtai <vo@manhtai.com>
2017-10-04 23:18:02 +0700

    Login placeholder

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
-
-	"github.com/gorilla/websocket"
 )
 
 // templ represents a single template
@@ -27,36 +25,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, r)
 }
 
-const (
-	socketBufferSize  = 1024
-	messageBufferSize = 256
-)
-
-var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
-	WriteBufferSize: socketBufferSize}
-
-func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	socket, err := upgrader.Upgrade(w, req, nil)
-	if err != nil {
-		log.Fatal("ServeHTTP:", err)
-		return
-	}
-	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
-	}
-	r.join <- client
-	defer func() { r.leave <- client }()
-	go client.write()
-	client.read()
-}
-
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the  application.")
 	flag.Parse()
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
